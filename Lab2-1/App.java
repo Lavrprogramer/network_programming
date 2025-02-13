90ddd08829159cd9d6e409b9cdfe13c29df16ef5
Lavr <nomi283623@gmail.com>
2025-02-13 14:35:48 +0200

    Зроблено Завдання №1

---
+++ Lab2-1/App.java
@@ -0,0 +1,21 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class App {
+
+    public static void main(String[] args) throws IOException
+{ 
+ BufferedReader reader = new BufferedReader( new InputStreamReader(System.in));
+
+  System.out.println("Як вас звати? ");
+  String Name = (reader.readLine()); 
+
+  System.out.println("Привiт" + Name);
+
+  System.out.println("Скiльки вам рокiв ?");
+  int Age = Integer.parseInt(reader.readLine());
+  int year = 2025 - Age;
+  System.out.println("Ти народився в : " + year); 
+}
+}
