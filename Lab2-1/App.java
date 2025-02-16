0cbc1dc15c3b15fa85859fefecc86e739d355375
Lavr <nomi283623@gmail.com>
2025-02-16 18:56:18 +0200

    add app2

--- Lab2-1/App.java
+++ Lab2-1/App.java
@@ -3,7 +3,7 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 
 public class App {
-
+  
     public static void main(String[] args) throws IOException
 { 
  BufferedReader reader = new BufferedReader( new InputStreamReader(System.in));
